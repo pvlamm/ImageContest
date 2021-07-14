1f51804471101362d5796423e26c8a4e42cba5d5
Patrick Lamm <lammster@gmail.com>
2021-07-13 23:38:11 -0400

    Implementing some services

---
+++ cli/ImageSource/Services/ImageFileService.cs
@@ -0,0 +1,46 @@
+﻿using ImageContest.Application.Common.Interfaces;
+using ImageContest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageSource.Services
+{
+    public class ImageFileService : IImageFileService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ImageFileService(IApplicationDbContext context)
+        {
+            _context = context ?? throw new NullReferenceException(nameof(context));
+        }
+
+        public IEnumerable<ImageFolder> GetImageFolders(string rootFolder)
+        {
+            if (string.IsNullOrWhiteSpace(rootFolder))
+            {
+                throw new ArgumentNullException($"{nameof(rootFolder)} parameter is null");
+            }
+
+            var rootDirectory = new DirectoryInfo(rootFolder);
+
+            var root = _context.Roots
+                .Include(a => a.Folders)
+                    .ThenInclude(a => a.ImageSets)
+                .FirstOrDefault(a => a.Name == rootDirectory.Name)
+                ?? new Root { Name = rootDirectory.Name, RootFolder = rootDirectory.FullName };
+
+            return GetImageFolders(root);
+        }
+
+        public IEnumerable<ImageFolder> GetImageFolders(Root rootFolder)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ lib/ImageContest.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,96 @@
+﻿using ImageContest.Application.Common.Interfaces;
+using ImageContest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ImageContest.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<Root> Roots { get; set; }
+        public DbSet<ImageFolder> ImageFolders { get; set; }
+        public DbSet<ImageSet> ImageSets { get; set; }
+        public DbSet<Contest> Contests { get; set; }
+        public DbSet<ContestOption> ContestOptions { get; set; }
+        public DbSet<ContestVote> ContestVotes { get; set; }
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        IModel IApplicationDbContext.Model => base.Model;
+        private IDbContextTransaction _currentTransaction;
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_currentTransaction != null)
+            {
+                return;
+            }
+
+            _currentTransaction = await base.Database.BeginTransactionAsync(IsolationLevel.ReadCommitted).ConfigureAwait(false);
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await SaveChangesAsync().ConfigureAwait(false);
+
+                _currentTransaction?.Commit();
+            }
+            catch
+            {
+                RollbackTransaction();
+                throw;
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public void RollbackTransaction()
+        {
+            try
+            {
+                _currentTransaction?.Rollback();
+            }
+            finally
+            {
+                if (_currentTransaction != null)
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
